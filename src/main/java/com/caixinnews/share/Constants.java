28b29523707247bcc2e1bd5e0085da33fd457912
lingdujiang <jqm_fyd@163.com>
2017-02-21 12:34:44 +0800

    Update Constants.java
    更改Constants初始化值

--- src/main/java/com/caixinnews/share/Constants.java
+++ src/main/java/com/caixinnews/share/Constants.java
@@ -2,9 +2,9 @@ package com.caixinnews.share;
 
 public class Constants {
 	// 各平台的app_id
-    public static final String APP_ID = "wx1cc00ab56e0ae378";//微信
-	public static final String APP_WECHAT_SECRET = "1391f530f7e2ed17d28701947b74808a";
-    public static final String APP_ID_QQ = "100761191";//QQ
+    public static String APP_ID = "";//微信//wx1cc00ab56e0ae378
+	public static String APP_WECHAT_SECRET = "";//1391f530f7e2ed17d28701947b74808a
+    public static String APP_ID_QQ = "";//QQ//100761191
 
     
 //    public static class ShowMsgActivity {
@@ -13,9 +13,9 @@ public class Constants {
 //		public static final String BAThumbData = "showmsg_thumb_data";
 //	}
 
-	public static final String APP_KEY_WEIBO = "3059582272";
-	public static final String REDIRECT_URL_WEIBO = "http://iphone.caixin.cn/app/weibo/callback.php";//应用回调页
-	public static final String SCOPE_WEIBO = "email,direct_messages_read,direct_messages_write,"
-			+ "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
-			+ "follow_app_official_microblog," + "invitation_write";//应用申请的高级权限
+	public static String APP_KEY_WEIBO = "";//3059582272
+	public static String REDIRECT_URL_WEIBO = "";//应用回调页//http://iphone.caixin.cn/app/weibo/callback.php
+	public static String SCOPE_WEIBO = "";//应用申请的高级权限//email,direct_messages_read,direct_messages_write,"
+//	+ "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
+//			+ "follow_app_official_microblog," + "invitation_write
 }
